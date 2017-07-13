9407005d33a53a0143f3e6cb48b86cc5cccdf21e
huizi <sunshineyao.aiesec@gmail.com>
2017-07-13 10:55:15 -0400

    Create Ex12.py

---
+++ Ex12.py
@@ -0,0 +1,17 @@
+# -- coding: utf-8 --
+#eval() interactive shell,interprets a string as code
+
+y = input("Name?")
+print(y)
+
+age = input("How old are you?")
+height = input ("how tall are you?")
+weight = input ("how much do you weigh?")
+
+print("So, you're %r old, %r tall and %r heavy."%(age,height,weight))
+
+#code in terminal (get documentation)
+'''
+python -m pydoc input 
+
+'''
