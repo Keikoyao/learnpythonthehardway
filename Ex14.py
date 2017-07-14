ab38502e92bdaee35d22a7b311831da2ee603b2b
huizi <sunshineyao.aiesec@gmail.com>
2017-07-14 13:10:31 -0400

    interactive input with user

--- Ex14.py
+++ Ex14.py
@@ -2,13 +2,13 @@
 from sys import argv
 script,user_name = argv
 prompt = '>>>'
-print("Hi %s,I'm the %s script."%(user_name,script))
+print("Hi %s,I'm the %s script."% (user_name,script))
 print("I'd like to ask you a few questions.")
 
-print("Do you like me %s?"%user_name)
+print("Do you like me %s?"% user_name)
 likes = input(prompt)
 
-print("Where do you live %s?" %user_name)
+print("Where do you live %s?" % user_name)
 lives = input(prompt)
 
 print("what kind of computer do you have?")
