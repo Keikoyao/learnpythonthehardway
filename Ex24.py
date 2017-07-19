dc0a26ad7e8aa9dc0fa68f91ec47c0fc88da54b6
huizi <sunshineyao.aiesec@gmail.com>
2017-07-19 15:35:34 -0400

    practice

--- Ex24.py
+++ Ex24.py
@@ -1,4 +1,16 @@
 # -- coding: utf-8 --
+print ("Let's practice everything.")
+print ('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
+
+poem = """ 
+\tThe lovely world 
+with logic so firmly planted 
+cannot discern \n the needs of love 
+nor comprehend passion from intuition 
+and requires an explanation 
+\n\t\twhere there is none. 
+"""
+
 print ("------------")
 print (poem)
 print ("------------")
