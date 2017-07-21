eb11c4a185239fc78bde674b1897cfd7d96131d4
huizi <sunshineyao.aiesec@gmail.com>
2017-07-21 14:14:15 -0400

    Dict {key:value}

--- Ex40.py
+++ Ex40.py
@@ -3,6 +3,7 @@ cities = {'CA':'San Francisco','MI':'Detroit','FL':'Jacksonville'}
 cities['NY']='New York'
 cities['OR']='Portland'
 
+#A function is also a variable
 def find_city(themap,state):
     if state in themap:
         return themap[state]
@@ -20,4 +21,15 @@ while True:
     #this line is the most important ever! Study!
     city_found = cities['_find'](cities,state)
     print(city_found)
+    
+#we put a function find_city into a dict with a key '_find'
+cities['_find'] = find_city
+#create another variable coty_found, cities is a dict, with a key '_find' 
+#find the value--find_city,which is a function. 
+#2 parameters cities, state will be passed on to the function find_city
+city_found = cities['_find'](cities,state)
+#function find_city will try to find states(key) from cities and return the value
+#if there is no match, return "Not found"
+#find_city gives the value returned to the variable city_found
+
         
